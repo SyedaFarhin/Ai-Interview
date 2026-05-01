241238d5ccdab0c69b7b0c4935209142ffe6a6e7
Syeda Farhin <syedafarhin18@gmail.com>
2026-05-02 00:51:29 +0530

    connect frontend to backend URL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,66 +1,46 @@
 import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: API_URL,
     withCredentials: true
 })
 
 export async function register({ username, email, password }) {
-
     try {
         const response = await api.post('/api/auth/register', {
             username, email, password
         })
-
         return response.data
-
     } catch (err) {
-
         console.log(err)
-
     }
-
 }
 
 export async function login({ email, password }) {
-
     try {
-
         const response = await api.post("/api/auth/login", {
             email, password
         })
-
         return response.data
-
     } catch (err) {
         console.log(err)
     }
-
 }
 
 export async function logout() {
     try {
-
         const response = await api.get("/api/auth/logout")
-
         return response.data
-
-    } catch (err) {
-
-    }
+    } catch (err) {}
 }
 
 export async function getMe() {
-
     try {
-
         const response = await api.get("/api/auth/get-me")
-
         return response.data
-
     } catch (err) {
         console.log(err)
     }
-
 }
