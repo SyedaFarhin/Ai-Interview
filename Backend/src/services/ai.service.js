e47ccf4e82d087128da5ad6e2363e1138eae8900
Syeda Farhin <syedafarhin18@gmail.com>
2026-05-11 01:18:05 +0530

    Fix Puppeteer for Render deployment

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -58,12 +58,21 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+
     const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+
+    await page.setContent(htmlContent, {
+        waitUntil: "networkidle0"
+    })
 
     const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
+        format: "A4",
+        margin: {
             top: "20mm",
             bottom: "20mm",
             left: "15mm",
