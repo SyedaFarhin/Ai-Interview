190ee64ad5243bec3107a541ef4126c99897a11f
Syeda Farhin <syedafarhin18@gmail.com>
2026-05-10 22:37:27 +0530

    Fix frontend API base URL for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,11 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None"
+})
 
 
     res.status(201).json({
@@ -91,7 +95,11 @@ async function loginUserController(req, res) {
             { expiresIn: "1d" }
         )
 
-        res.cookie("token", token)
+        res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None"
+})
 
         res.status(200).json({
             message: "User loggedIn successfully.",
